c47365fb5781edfee8acecc0b44d96de974dbe3b
Ryoo Nishida <cu_0918@yahoo.co.jp>
2021-09-23 23:32:21 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 西田遼央 on 2021/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
