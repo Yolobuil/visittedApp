cfa721f318c9536c6f79cf13a8397167e95eb2cf
Ryoo Nishida <cu_0918@yahoo.co.jp>
2021-09-24 23:08:53 +0900

    commit

---
+++ FirstApp/test.playground/Contents.swift
@@ -0,0 +1,26 @@
+print("Hello, World!")
+
+var variable = 30
+variable = 60
+
+var b = "kon"
+
+let constant = 60
+// constantは定数なため変更できない
+
+var implicitInteger = 59
+
+print(type(of:implicitInteger))
+
+var implicitString = "ringo"
+
+print(type(of:implicitString))
+
+
+var a:String = "budo"
+var d:Int = 2
+var c:Bool = true
+
+String(d)
+
+String(c)
